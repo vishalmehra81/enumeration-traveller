48d1e01a2b4ae304457c6a6113f115c48c9f21a3
Vishal Mehra <67109899+vishalmehra81@users.noreply.github.com>
2020-08-20 08:10:13 +0100

    initial commit

---
+++ models/traveller.js
@@ -0,0 +1,42 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  }); 
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation;
+  });
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+ return this.journeys.filter((journey) => {
+   return journey.transport === transport
+ });
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance;
+  })
+
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance;
+  },0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  
+
+};
+
+
+module.exports = Traveller;
